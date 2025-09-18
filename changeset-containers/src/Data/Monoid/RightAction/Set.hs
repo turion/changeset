6c210c4a6d46ee49babb9c1e380d7e28e35f630d
Manuel Bärenz <mbaerenz@heilmannsoftware.de>
2025-09-18 10:46:34 +0200

    Add more changes for maps and sets

--- changeset-containers/src/Data/Monoid/RightAction/Set.hs
+++ changeset-containers/src/Data/Monoid/RightAction/Set.hs
@@ -6,12 +6,18 @@ import Data.Set
 -- changeset
 import Data.Monoid.RightAction (RightAction (..))
 
--- | Insert or delete an element in a 'Set'.
+-- | Insert or delete an element in a 'Set', or calculate the intersection, union, or difference with another set.
 data SetChange k
   = Insert k
   | Delete k
+  | Intersection (Set k)
+  | Union (Set k)
+  | Difference (Set k)
   deriving (Show, Read, Eq, Ord)
 
 instance (Ord k) => RightAction (SetChange k) (Set k) where
   actRight s (Insert k) = insert k s
   actRight s (Delete k) = delete k s
+  actRight s1 (Intersection s2) = intersection s1 s2
+  actRight s1 (Union s2) = s1 `union` s2
+  actRight s1 (Difference s2) = difference s1 s2
