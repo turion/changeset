dc86ab53a21ce752be17a9a23a8eaa789308c846
Manuel Bärenz <mbaerenz@heilmannsoftware.de>
2025-12-16 10:16:12 +0100

    Bump time to 1.15 and add new functionality

--- changeset-time/src/Data/Monoid/RightAction/Time/Orphans.hs
+++ changeset-time/src/Data/Monoid/RightAction/Time/Orphans.hs
@@ -4,6 +4,9 @@ module Data.Monoid.RightAction.Time.Orphans where
 
 -- time
 import Data.Time
+import Data.Time.Calendar
+import Data.Time.Clock.TAI
+import Data.Time.Local
 
 -- changeset
 import Data.Monoid.RightAction
@@ -13,3 +16,17 @@ instance RightAction NominalDiffTime UTCTime where
 
 instance RightTorsor NominalDiffTime UTCTime where
   differenceRight = diffUTCTime
+
+instance RightAction NominalDiffTime LocalTime where
+  actRight = flip addLocalTime
+
+instance RightAction DiffTime AbsoluteTime where
+  actRight time diffTime = addAbsoluteTime diffTime time
+
+-- | Uses 'addGregorianDurationRollOver'.
+instance RightAction CalendarDiffDays Day where
+  actRight time diffTime = addGregorianDurationRollOver diffTime time
+
+-- | Adds a number of days
+instance RightAction Integer Day where
+  actRight time diffTime = addDays diffTime time
